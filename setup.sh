a609c9d05d89f8d891271d3aa66e7fc51d3e2524
arubioji <adrian.rubio.jimenez@cern.ch>
2024-04-15 14:57:09 +0200

    Updating setup.sh: upgrading to 0.2.5 version

--- setup.sh
+++ setup.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
 setupATLAS -q
-#asetup StatAnalysis,0.2.5
-asetup StatAnalysis,0.0.4
+asetup StatAnalysis,0.2.5
+#asetup StatAnalysis,0.0.4
+
+## Versions from 0.3.0 need AlmaLinux
+#setupATLAS -c el9
+#asetup StatAnalysis,0.3.0
+
 export VLQCOMBDIR=$PWD
 export PATH=$VLQCOMBDIR/bin/:$VLQCOMBDIR/python/:$PATH
 export PYTHONPATH=$VLQCOMBDIR/python/:$VLQCOMBDIR/VLQ_Interpretation_Tools/:$PYTHONPATH
